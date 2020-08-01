afa90a0d1e551c790e33eeba4b011924fa4f0717
bernethe <bernethe@bernethe.local>
2020-07-31 22:04:43 -0600

    Sólo agosto

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ function cambioPlaca() {
 	document.getElementById('cal_y').dataset.placa = _s;
 	document.getElementById('cal_o').dataset.placa = _s;
 
-	var _eleN = (Math.ceil(_s/2))-1;
-	if(_s == 0) {
-		_eleN = 4;
-	}
+	// var _eleN = (Math.ceil(_s/2))-1;
+	// if(_s == 0) {
+	// 	_eleN = 4;
+	// }
 
-	for(var i = 0; i < 7; i++) {
-		document.querySelectorAll('#semana .card')[i].classList.remove('card-y');
-		document.querySelectorAll('#semana .card')[i].classList.remove('card-r');
-		document.querySelectorAll('#semana .card')[i].classList.remove('card-g');
-	}
+	// for(var i = 0; i < 7; i++) {
+	// 	document.querySelectorAll('#semana .card')[i].classList.remove('card-y');
+	// 	document.querySelectorAll('#semana .card')[i].classList.remove('card-r');
+	// 	document.querySelectorAll('#semana .card')[i].classList.remove('card-g');
+	// }
 
-	document.querySelectorAll('#semana .card')[_eleN].classList.add('card-r');
-	document.querySelectorAll('#semana .card-wknd')[_s%2].classList.add('card-r');
+	// document.querySelectorAll('#semana .card')[_eleN].classList.add('card-r');
+	// document.querySelectorAll('#semana .card-wknd')[_s%2].classList.add('card-r');
 
 	document.getElementById('resultado').classList.remove('d-none');
 	
