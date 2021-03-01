6051cdf7d54be6892bf2b6ff909672a096ce52d5
Harold Bernethe Soto González <bernethe@gmail.com>
2021-03-01 12:13:21 -0600

    Cambios marzo 2021

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 'use strict';
 
+// var _now;
+// var _day = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 
 function main() {
 	document.getElementById('placa').addEventListener('change',cambioPlaca,!1);
 }
 function cambioPlaca() {
-	var _s = parseInt(document.getElementById('placa').value);
+	var _s = document.getElementById('placa').value;
 
-	var _eleN = (Math.ceil(_s/2))-1;
-	if(_s == 0) {
-		_eleN = 4;
-	}
+	document.getElementById('placaEnd').innerHTML = _s;
 
-	for(var i = 0; i < 7; i++) {
-		document.querySelectorAll('#semana .card')[i].classList.remove('card-y');
-		document.querySelectorAll('#semana .card')[i].classList.remove('card-r');
-		document.querySelectorAll('#semana .card')[i].classList.remove('card-g');
-	}
-
-	document.querySelectorAll('#semana .card')[_eleN].classList.add('card-r');
-	//document.querySelectorAll('#semana .card-wknd')[_s%2].classList.add('card-r');
-
-	document.getElementById('cal_y').dataset.placa = _s;
-	document.getElementById('placa_show').innerHTML = _s;
-	document.getElementById('placa_show_cal').innerHTML = _s;
+	document.getElementById('semana').dataset.placa = _s;
 
 	document.getElementById('resultado').classList.remove('d-none');
 	
