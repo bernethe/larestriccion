aa4bc91999ba0a3e53c6766d6aec48f935284c60
Harold Bernethe Soto González <bernethe@gmail.com>
2021-07-19 10:02:45 -0600

    Muestre la fecha de hoy

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 'use strict';
 
-// var _now;
-// var _day = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+var _now, _day, _month;
 
 function main() {
+	_day = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+	_month = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'];
+	_now = new Date();
 	document.getElementById('placa').addEventListener('change',cambioPlaca,!1);
 }
 function cambioPlaca() {
 	var _s = document.getElementById('placa').value;
 
 	document.getElementById('placaEnd').innerHTML = _s;
+
+	document.getElementById('todayDate').innerHTML = _day[_now.getDay()]+' '+_now.getDate()+' de '+_month[_now.getMonth()];
 
 	document.getElementById('semana').dataset.placa = _s;
 
