5b814dcd7b75ad503db9992a086ee81d7fd80824
Harold Bernethe Soto González <bernethe@gmail.com>
2021-08-29 18:35:35 -0600

    Cambios para Septiembre

--- v4/script.js
+++ v4/script.js
@@ -8,14 +8,13 @@ function cambioPlaca() {
 	document.getElementById('placaEnd').innerHTML = _s;
 	document.getElementById('todayDate').innerHTML = _day[_now.getDay()]+' '+_now.getDate()+' de '+_month[_now.getMonth()];
 	document.getElementById('semana').dataset.placa = _s;
-	// document.getElementById('calendario').dataset.placa = _s;
+	document.getElementById('calendario').dataset.placa = _s;
 	document.getElementById('resultado').classList.remove('d-none');
 	
 }
 
 function cambioContraste() {
-	// document.getElementById('calendario').classList.toggle('contrast');
-	document.getElementById('semana').classList.toggle('contrast');
+	document.getElementById('calendario').classList.toggle('contrast');
 	var toChange = document.getElementsByClassName('pc');
 	for(var i = 0;i < toChange.length; i++) {
 		toChange[i].classList.toggle('contrast');
